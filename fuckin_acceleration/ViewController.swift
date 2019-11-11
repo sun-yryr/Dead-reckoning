5f56813b0046551115440b5779ea3ca4a9cc8607
sun-yryr <taittide@gmail.com>
2019-11-11 21:34:00 +0900

    Initial Commit

---
+++ fuckin_acceleration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fuckin_acceleration
+//
+//  Created by 皆川泰陽 on 2019/11/11.
+//  Copyright © 2019 皆川泰陽. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
