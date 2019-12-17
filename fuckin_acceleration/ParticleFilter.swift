5ff0b71ba43d5a94e6d0c162564b8838f892bb52
sun-yryr <taittide@gmail.com>
2019-12-17 22:09:34 +0900

    margeミス

--- fuckin_acceleration/ParticleFilter.swift
+++ fuckin_acceleration/ParticleFilter.swift
@@ -41,7 +41,7 @@ class ParticleFilter {
     /*     ここからprivate      */
     
     private static func generate_random_particles(particleCount: Int) -> Array<Array<Double>> {
-        var particles = Array(repeating: Array(repeating: 0.0, count: 4), count: particleCount)
+        var particles = Array(repeating: Array(repeating: 0.0, count: 6), count: particleCount)
         for i in particles.indices {
             for j in particles[0].indices {
                 particles[i][j] = Double.random(in: -1 ... 1)
@@ -53,8 +53,8 @@ class ParticleFilter {
     private func move_particles(particles: [[Double]], seconds: Int = 1) -> [[Double]] {
         var particles = particles
         for i in particles.indices {
-            for j in 0..<2 {
-                particles[i][j] += particles[i][j+2]
+            for j in 0..<3 {
+                particles[i][j] += particles[i][j+3]
             }
         }
         return particles
@@ -62,7 +62,7 @@ class ParticleFilter {
     
     private func calurate_likelihood(particles: [[Double]], measuredPoint: [Double]) -> [Double] {
         let calGauss = { (particle: [Double]) -> Double in
-            let left = 1.0/(pow(self.sigma, 4) * pow((2 * Double.pi), 2))
+            let left = 1.0/(pow(self.sigma, 6) * pow((2 * Double.pi), 3))
             var right = zip(particle, measuredPoint).reduce(0.0) { (Result, arg1) -> Double in
                 let (p, m) = arg1
                 return Result + pow((m - p), 2)
